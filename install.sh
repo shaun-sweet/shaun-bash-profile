e6f0ee74b2e4ced50e6524e686cfb4521ce837e8
Shaun Sweet <shaun@shaunsweet.com>
2018-05-06 15:51:22 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 DOWNLOADED_DIR=`pwd`;
 
-cd ~;
-mkdir ~/bash-settings;
-mv $DOWNLOADED_DIR/* ~/bash-settings/
 echo "BASH_SETTINGS=~/bash-settings/main.sh" >> ~/.bash_profile;
 echo "if [ -f \"$BASH_SETTINGS\" ]; then" >> ~/.bash_profile;
 echo ". \"$BASH_SETTINGS\";" >> ~/.bash_profile
 echo "fi" >> ~/.bash_profile
 
 echo "echo \"Bash settings loaded!\";" >> ~/.bash_profile
+mkdir ~/bash-settings;
+mv $DOWNLOADED_DIR/* ~/bash-settings/
+cd ~;
+
+source ~/.bash_profile;
