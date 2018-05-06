443d25b92326e9636e9962b02602951801f62f5d
Shaun Sweet <shaun@shaunsweet.com>
2018-05-06 15:44:08 -0700

    install fix

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 DOWNLOADED_DIR=`pwd`;
 
 cd ~;
-# mkdir ~/bash-settings;
-# mv $DOWNLOADED_DIR/* ~/bash-settings/
-echo "BASH_SETTINGS=~/bash-settings/main.sh
-if [ -f \"$BASH_SETTINGS\" ]; then
-. \"$BASH_SETTINGS\";
-fi
+mkdir ~/bash-settings;
+mv $DOWNLOADED_DIR/* ~/bash-settings/
+echo "BASH_SETTINGS=~/bash-settings/main.sh" >> ~/.bash_profile;
+echo "if [ -f \"$BASH_SETTINGS\" ]; then" >> ~/.bash_profile;
+echo ". \"$BASH_SETTINGS\";" >> ~/.bash_profile
+echo "fi" >> ~/.bash_profile
 
-echo \"Bash settings loaded!\";" >> ~/.bash_profile
+echo "echo \"Bash settings loaded!\";" >> ~/.bash_profile
